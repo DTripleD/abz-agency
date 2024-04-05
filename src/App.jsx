94c5154b46d25b342ce818dbd17187530cebd138
Danil Deineka <tripledpadik@gmail.com>
2024-04-05 12:17:48 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import Header from "components/Header/Header";
+
+import HeroSection from "components/HeroSection/HeroSection";
+import UsersSection from "components/UsersSection/UsersSection";
+import FormSection from "components/FormSection/FormSection";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <HeroSection />
+        <UsersSection />
+        <FormSection />
+      </main>
+    </div>
+  );
+}
+
+export default App;
